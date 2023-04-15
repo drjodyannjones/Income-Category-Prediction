bf5405c7a4fde270fb0dd2cef3eae4fb31bf5754
Jody-Ann Jones <drjodyannjones@Jody-Anns-MBP.lan>
2023-04-15 01:42:47 -0500

    Add test_model.py

---
+++ src/tests/test_model.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from src.app.models.model import train_model, compute_model_metrics, inference
+
+
+def test_train_model():
+    X_train = np.array([[1, 2], [3, 4]])
+    y_train = np.array([0, 1])
+
+    model = train_model(X_train, y_train)
+    assert model is not None, "Model not returned"
+
+
+def test_compute_model_metrics():
+    y = np.array([1, 0, 1, 0])
+    preds = np.array([1, 0, 0, 1])
+
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+
+    assert precision == 0.5, "Incorrect precision"
+    assert recall == 0.5, "Incorrect recall"
+    assert fbeta == 0.5, "Incorrect F1 score"
+
+
+def test_inference():
+    X = np.array([[1, 2], [3, 4]])
+    model = train_model(X, np.array([0, 1]))
+    preds = inference(model, X)
+    print(preds)
+
+    assert len(preds) == 2, "Incorrect number of predictions"
+    assert all(
+        isinstance(p, (int, float)) or np.issubdtype(p, np.number) for p in preds
+    ), "Predictions should be integers or floats"
